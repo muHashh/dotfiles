7a94ae8d3a5e81fc81b5fcabe5899d92aa95e6fd
Hassan Shahid <muhashh@protonmail.com>
2023-12-18 22:36:46 +0100

    change some default neovim keymap config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.api.nvim_set_keymap
+local delete = vim.keymap.del
+
+delete("n", "<S-h>")
+delete("n", "<S-l>")
+
+map("n", "<C-S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+map("n", "<C-S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
