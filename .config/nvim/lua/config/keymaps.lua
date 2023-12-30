227859852a8af3e0754cc61d6fbd4705ae730cd2
Hassan Shahid <muhashh@protonmail.com>
2023-12-30 21:33:27 +0100

    global: bunch of updates

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,8 @@ delete("n", "<S-h>")
 delete("n", "<S-l>")
 map("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+
+-- Terminal
+map("n", "<leader>tt", "<cmd>lcd %:p:h | term<cr>", { desc = "Create terminal (buffer dir)" })
+map("n", "<leader>th", "<cmd>split | lcd %:p:h | term<cr>", { desc = "Create horizontal terminal (buffer dir)" })
+map("n", "<leader>tv", "<cmd>vsplit | lcd %:p:h | term<cr>", { desc = "Create vertical terminal (buffer dir)" })
