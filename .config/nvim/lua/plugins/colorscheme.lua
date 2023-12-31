0c77fbe5db0e8bf6fa09b94c6b2e6685b3e13e44
Hassan Shahid <muhashh@protonmail.com>
2023-12-31 22:51:28 +0100

    global: change to github dark theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,16 @@ return {
   -- { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
   { "sainnhe/gruvbox-material" },
   {
+    "projekt0n/github-nvim-theme",
+    lazy = false,
+    priority = 1000,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "gruvbox-material",
-      colorscheme = "oxocarbon",
+      -- colorscheme = "oxocarbon",
+      colorscheme = "github_dark_default",
       -- background = "dark",
     },
   },
